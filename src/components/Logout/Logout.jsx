dd31ff950f7756febdec49cc56384e0ee8740153
Akinwale Samuel <wale0331@gmail.com>
2019-05-28 17:52:17 +0100

    166196197-feature(logout): add logout functionality

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Logout = () => (
-  <div className="logout">
-    <button type="submit" id="user-icon"><i className="fas fa-user" /></button>
-    <button type="submit" id="logout">Logout</button>
-  </div>
-);
+class Logout extends Component {
+  logoutHandler = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('who');
+    localStorage.removeItem('picture');
+    localStorage.removeItem('email');
+    localStorage.removeItem('name');
+    localStorage.removeItem('phone');
+
+    window.location = '/';
+  };
+
+  render() {
+    return (
+      <div className="logout">
+        <button type="submit" id="user-icon"><i className="fas fa-user" /></button>
+        <button type="submit" id="logout" onClick={this.logoutHandler}>Logout</button>
+      </div>
+    );
+  }
+}
 
 export default Logout;
