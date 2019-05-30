c7643049ca761fb41733abdd4bedef92ee59ceed
Akinwale Samuel <wale0331@gmail.com>
2019-05-30 08:05:19 +0100

    166347568-fix(menu): add responsiveness to the menu bar

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -11,38 +11,56 @@ const Menu = () => {
   if (token) {
     if (privilege === 'true') {
       return (
-        <nav>
-          <div className="pry-menu-items">
-            <Link to="/parties">Create Party</Link>
-            <Link to="/create-office">Create Office</Link>
+        <div>
+          <nav>
+            <div className="pry-menu-items">
+              <Link to="/parties">Create Party</Link>
+              <Link to="/create-office">Create Office</Link>
+            </div>
+            <Logout />
+          </nav>
+          <div className="mini-menu-icon">
+            <img src="https://res.cloudinary.com/walsam/image/upload/v1559199507/politico-mini.png" alt="logo" />
+            <button type="submit"><i className="fas fa-bars" /></button>
           </div>
-          <Logout />
-        </nav>
+        </div>
       );
     }
     return (
-      <nav>
-        <div className="pry-menu-items">
-          <Link to="/parties">Parties</Link>
-          <Link to="/candidates">Vote</Link>
-          <Link to="/history">Voting History</Link>
+      <div>
+        <nav>
+          <div className="pry-menu-items">
+            <Link to="/parties">Parties</Link>
+            <Link to="/candidates">Vote</Link>
+            <Link to="/history">Voting History</Link>
+          </div>
+          <div className="logout">
+            <Logout />
+          </div>
+        </nav>
+        <div className="mini-menu-icon">
+          <img src="https://res.cloudinary.com/walsam/image/upload/v1559199507/politico-mini.png" alt="logo" />
+          <button type="submit"><i className="fas fa-bars" /></button>
         </div>
-        <div className="logout">
-          <Logout />
-        </div>
-      </nav>
+      </div>
     );
   }
   return (
-    <nav>
-      <div className="pry-menu-items">
-        <Link to="/parties">Parties</Link>
+    <div>
+      <nav>
+        <div className="pry-menu-items">
+          <Link to="/parties">Parties</Link>
+        </div>
+        <div className="logout">
+          <Link to="/login" id="login">Login</Link>
+          <Link to="/register" id="signup">Register</Link>
+        </div>
+      </nav>
+      <div className="mini-menu-icon">
+        <img src="https://res.cloudinary.com/walsam/image/upload/v1559199507/politico-mini.png" alt="logo" />
+        <button type="submit"><i className="fas fa-bars" /></button>
       </div>
-      <div className="logout">
-        <Link to="/login" id="login">Login</Link>
-        <Link to="/register" id="signup">Register</Link>
-      </div>
-    </nav>
+    </div>
   );
 };
 
